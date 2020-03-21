c565fc64beff86008e2e33bbee0640d6c0fad49d
tangshuimei <995170180@qq.com>
2020-03-21 21:34:10 +0800

    5-1nav-header组件

--- src/App.vue
+++ src/App.vue
@@ -17,31 +17,11 @@ export default {
    
   },
   mounted () {
-    // 本地加载请求静态json文件的形式
-    // this.axios.get('/mock/user/login.json',(res)=>{
-    //   this.res = res
-    // })
-    // 通过easy-mock平台实现数据mock
-    // this.axios.get('/user/login',(res)=>{
-    //   this.res = res
-    // })
-    // 本地集成mockjs实现数据mock
-    this.axios.get('/user/login').then(res=>{
-      // res获取到的是data的数据，因此没有status
-      
-      this.res = res
-    })
+   
   },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss';
 </style>
--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,6 +1,48 @@
 <template>
-    <div>
-        navheader
+    <div class="header">
+        <div class="nav-topbar">
+            <div class="container">
+                <div class="topbar-menu">
+                    <a href="javascript:;">小米商城</a>
+                    <a href="javascript:;">MUI</a>
+                    <a href="javascript:;">云服务</a>
+                    <a href="javascript:;">协议规则</a>
+                </div>
+                <div class="topbar-user">
+                    <a href="javascript:;">登录</a>
+                    <a href="javascript:;">注册</a>
+                    <a href="javascript:;" class="my-cart"><span class="icon-cart"></span> 购物车</a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-header">
+            <div class="container">
+                <div class="header-logo">
+                    <a href="/#/index" class=""></a>
+                </div>
+                <div class="header-menu">
+                    <div class="item-menu">
+                        <span>小米手机</span>
+                        <div class="children">
+
+                        </div>
+                    </div>
+                    <div class="item-menu">
+                        <span>Redmi红米</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>电视</span>
+                        <div class="children"></div>
+                    </div>
+                </div>
+                <div class="header-search">
+
+                </div>
+
+            </div>
+
+        </div>
     </div>
 </template>
 <script>
@@ -9,3 +51,105 @@
 
     }
 </script>
+<style lang="scss">
+    @import './../assets/scss/base.scss';
+    .header{
+        .nav-topbar{
+            height: 39px;
+            line-height: 39px;
+            background-color: #333333;
+            color: #B0B0B0;
+            .container{
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                a{
+                    display: inline-block;
+                    color: #B0B0B0;
+                    margin-right: 14px; 
+                }
+                .my-cart{
+                    width: 110px;
+                    background-color: #FF6600;
+                    text-align: center;
+                    color: #fff;
+                    .icon-cart{
+                        display: inline-block;
+                        width: 16px;
+                        height: 12px;
+                        margin-right: 4px;
+                        background: url('/imgs/icon-cart-checked.png') no-repeat center;
+                        background-size: contain;
+                    }
+                }
+            }
+        }
+        .nav-header{
+            position: relative;
+            left: 0;
+            right: 0;
+            .container{
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                height: 112px;
+                .header-logo{
+                    display: inline-block;
+                    width: 55px;
+                    height: 55px;
+                    background-color: #FF6600;
+                    a{
+                        display: inline-block;
+                        width: 110px;
+                        height: 55px;
+                        &:before{
+                            content: " ";
+                            display: inline-block;
+                            width: 55px;
+                            height: 55px;
+                            background: url('/imgs/mi-logo.png') no-repeat center;
+                            background-size: 55px;
+                            transition: margin .2s;
+                        }
+                        &:after{
+                            content: " ";
+                            display: inline-block;
+                            width: 55px;
+                            height: 55px;
+                            background: url('/imgs/home-logo.png') no-repeat center;
+                            background-size: 55px;
+                        }
+                        &:hover::before{
+                            margin-left: -55px;
+                            transition: margin .2s;
+                        }
+                    }
+                }
+                .header-menu{
+                    display: inline-block;
+                    width: 200px;
+                    padding-left: 209px;
+                    .item-menu{
+                        display: inline-block;
+                        color: #333333;
+                        font-size: 16px;
+                        font-weight: bold;
+                        line-height: 112px;
+                        span{
+                            cursor: pointer;
+                            &:hover{
+                                
+                            }
+                        }
+                        .children{
+                            position: absolute;
+                            left: 0;
+                            right: 0;
+                            width: 100%;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
